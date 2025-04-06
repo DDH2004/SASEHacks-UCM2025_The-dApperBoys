7392c5f620d81704102f9c4a1d3bdf81158ab62f
Shyam-723 <shyam.gupta0723@gmail.com>
2025-04-05 17:50:23 -0700

    style changes

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -32,9 +32,6 @@ const Dashboard: React.FC<DashboardProps> = ({ wallet, onDisconnect }) => {
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="bg-zinc-800 rounded-lg shadow-xl p-6">
-          <h2 className="text-2xl font-bold text-white mb-4">
-            Submit Environmental Proof
-          </h2>
 
           <BarcodeScanner />
 
